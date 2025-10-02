aaa7aab3592ce95f10f17f417a1305a115faa49f
Jaros <jaroslavzev63@mail.ru>
2025-10-02 21:42:57 +0500

    workout

---
+++ analyze.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+// Берём первый позиционный аргумент или читаем из STDIN
+$args = $_SERVER['argv'];
+array_shift($args); // убрать имя скрипта
+
+$input = count($args) > 0 ? $args[0] : stream_get_contents(STDIN);
+
+// Уберём перевод строки в конце, если ввели из терминала/pipe
+$input = rtrim((string)$input, "\r\n");
+
+// Считаем длину максимально корректно для Юникода
+if (function_exists('grapheme_strlen')) {
+    // Требует расширение intl; считает графемные кластеры (видимые символы)
+    $len = grapheme_strlen($input);
+} elseif (function_exists('mb_strlen')) {
+    // Требует расширение mbstring; считает кодовые точки (символы)
+    mb_internal_encoding('UTF-8');
+    $len = mb_strlen($input);
+} else {
+    // Байтовая длина (как запасной вариант)
+    $len = strlen($input);
+}
+
+fwrite(STDOUT, (string)$len . PHP_EOL);
