57d35dee0db4136c186d3465e61e110c3ba45bf7
ImanBurluk <jaroslavzev63@mail.ru>
2025-10-16 22:37:35 +0500

    chore: Обновил textcount

--- bin/analyze.php
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/php7.4
-<?php
-
-declare(strict_types=1);
-
-require __DIR__ . '/../vendor/autoload.php';
-
-$len = consoleCalcLength($_SERVER['argv'] ?? null);
-outPrintln((string) $len);
