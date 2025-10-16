e31ab79cf6465195a4f9e035d7549708ed6cc932
ImanBurluk <jaroslavzev63@mail.ru>
2025-10-16 22:33:20 +0500

    chore: Добавил класс TextCount

---
+++ src/TextCount/TextCount.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Imanburluk\TextCount;
+
+use Imanburluk\TextCount\Console\ArgParser;
+use Imanburluk\TextCount\Console\Output;
+use Imanburluk\TextCount\LengthCalculator;
+
+class TextCount
+{
+    public function run(?array $argv): void
+    {
+        $parser = new ArgParser();
+        $calculator = new LengthCalculator();
+        $output = new Output();
+
+        $input = $parser->parse($argv);
+        $length = $calculator->calculate($input);
+        $output->println((string) $length);
+    }
+}
