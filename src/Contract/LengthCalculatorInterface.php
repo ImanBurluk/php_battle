c24a9e1e1077ad5ef4d116d78f20695c00bda654
ImanBurluk <jaroslavzev63@mail.ru>
2025-10-17 21:48:25 +0500

    chore: Выношу зависимости на интерфейсы (D — Dependency Inversion): добавил LengthCalculatorInterface.php

---
+++ src/Contract/LengthCalculatorInterface.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+namespace Imanburluk\TextCount\Contract;
+
+/**
+ * Считает длину входной строки.
+ */
+interface LengthCalculatorInterface
+{
+    public function calculate(string $input): int;
+}
