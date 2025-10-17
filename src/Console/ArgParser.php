3ee540a17de6ac13fc13b9c6e00beda3e1750d46
ImanBurluk <jaroslavzev63@mail.ru>
2025-10-17 21:51:23 +0500

    chore: Имплементация InputParserInterface классом ArgParser

--- src/Console/ArgParser.php
+++ src/Console/ArgParser.php
@@ -1,17 +1,21 @@
 <?php
-
 declare(strict_types=1);
 
 namespace Imanburluk\TextCount\Console;
 
-class ArgParser
+use Imanburluk\TextCount\Contract\InputParserInterface;
+
+/**
+ * Реализация парсера CLI-аргументов.
+ */
+final class ArgParser implements InputParserInterface
 {
     /**
-     * @param list<string>|null $argv  Аргументы командной строки: позиционный список строк
+     * @param list<string>|null $argv
      */
     public function parse(?array $argv): string
     {
-        if (is_array($argv) && isset($argv[1])) {
+        if (is_array($argv) && isset($argv[1]) && $argv[1] !== '') {
             return (string) $argv[1];
         }
 
