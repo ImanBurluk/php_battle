b88e2703ad462b868cf34d953525a567a5055868
ImanBurluk <jaroslavzev63@mail.ru>
2025-10-19 20:32:57 +0500

    chore: Допилен до ума тест ArgParserTest

--- tests/Unit/ArgParserTest.php
+++ tests/Unit/ArgParserTest.php
@@ -8,14 +8,14 @@ final class ArgParserTest extends TestCase
 {
     public function testParseFromArgv(): void
     {
-        $p = new ArgParser();
-        self::assertSame('hello', $p->parse(['script.php', 'hello']));
+        $parser = new ArgParser();
+        self::assertSame('hello', $parser->parse(['script.php', 'hello']));
     }
 
     public function testParseEmptyArgvFallsBackToStdin(): void
     {
         // Подменять STDIN сложно в юнит-тесте, поэтому проверим поведение с argv:
-        $p = new ArgParser();
-        self::assertSame('', $p->parse(['script.php', '']));
+        $parser = new ArgParser();
+        self::assertSame('', $parser->parse(['script.php', '']));
     }
 }
