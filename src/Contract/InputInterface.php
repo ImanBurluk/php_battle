4846d64344fcb57015d68c564a34c8717035a4e4
ImanBurluk <jaroslavzev63@mail.ru>
2025-10-21 22:16:33 +0500

    feat: Добавлен новый контракт (aka порт) чтения

---
+++ src/Contract/InputInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Imanburluk\TextCount\Contract;
+
+interface InputInterface
+{
+    public function read(): string;
+}
--- src/TextCount.php
+++ src/TextCount.php
@@ -1,18 +1,1 @@
-<?php
-
-// src/TextCount.php
-declare(strict_types=1);
-
-namespace Imanburluk\TextCount;
-
-use Imanburluk\TextCount\Contract\LengthCalculatorInterface;
-
-final class TextCount
-{
-    public function __construct(private LengthCalculatorInterface $calculator) {}
-
-    public function count(string $text): int
-    {
-        return $this->calculator->calculate($text);
-    }
-}
+<?phpdeclare(strict_types=1);namespace Imanburluk\TextCount\Contract;interface InputInterface{    public function read(): string;}
