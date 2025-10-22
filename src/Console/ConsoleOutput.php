52b163820e002503119fa871d4c9f1989bfb1689
ImanBurluk <jaroslavzev63@mail.ru>
2025-10-22 20:03:25 +0500

    feat: Добавлен адаптер вывода ConsoleOutput

---
+++ src/Console/ConsoleOutput.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace Imanburluk\TextCount\IO;
+
+use Imanburluk\TextCount\Contract\OutputInterface;
+
+final class ConsoleOutput implements OutputInterface
+{
+    public function write(string $line): void
+    {
+        echo $line, PHP_EOL;
+    }
+}
