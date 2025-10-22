fa3f613af700dec8e022c6cd473c94757546f5ff
ImanBurluk <jaroslavzev63@mail.ru>
2025-10-22 19:58:48 +0500

    feat: Изменён клиентский скрипт

--- bin/textcount.php
+++ bin/textcount.php
@@ -1,18 +1,1 @@
-#!/usr/bin/env php
-<?php
-
-declare(strict_types=1);
-
-require __DIR__ . '/../vendor/autoload.php';
-
-use Imanburluk\TextCount\TextCount;
-use Imanburluk\TextCount\LengthCalculator;
-use Imanburluk\TextCount\Console\ArgParser;
-use Imanburluk\TextCount\Console\Output;
-
-$parser     = new ArgParser();
-$calculator = new LengthCalculator();
-$output     = new Output();
-
-$app = new TextCount($parser, $calculator, $output);
-$app->run($argv);
+#!/usr/bin/env php<?phpdeclare(strict_types=1);require __DIR__ . '/../vendor/autoload.php';use Imanburluk\TextCount\MbLengthCalculator;use Imanburluk\TextCount\TextCount;use Imanburluk\TextCount\Console\ArgvInput;use Imanburluk\TextCount\Console\TextCountApp;use Imanburluk\TextCount\IO\ConsoleOutput;$app = new TextCountApp(    new ArgvInput($argv),    new TextCount(new MbLengthCalculator()),    new ConsoleOutput());$app->run();
