8df2c02d493bb653a4d44e761e1502815cd20c0c
ImanBurluk <jaroslavzev63@mail.ru>
2025-10-22 20:02:19 +0500

    feat: Изменён класс TextCount

--- src/TextCount.php
+++ src/TextCount.php
@@ -1,1 +1,1 @@
-<?phpdeclare(strict_types=1);namespace Imanburluk\TextCount\Contract;interface InputInterface{    public function read(): string;}
+<?phpdeclare(strict_types=1);namespace Imanburluk\TextCount;use Imanburluk\TextCount\Contract\LengthCalculatorInterface;final class TextCount{    private LengthCalculatorInterface $calculator;    public function __construct(LengthCalculatorInterface $calculator)    {        $this->calculator = $calculator;    }    public function count(string $text): int    {        return $this->calculator->calculate($text);    }}
