7bae2f146c4b6cd87797ec3da160a40579f91c5f
ImanBurluk <jaroslavzev63@mail.ru>
2025-10-23 21:18:10 +0500

    refactor: Исправил phpdocs в ArgvInput

--- src/Console/ArgvInput.php
+++ src/Console/ArgvInput.php
@@ -8,17 +8,17 @@ use Imanburluk\TextCount\Contract\InputInterface;
 
 final class ArgvInput implements InputInterface
 {
-    /** @var list<string> $argv */
-    private array $argv;
+    /** @var list<string>|null */
+    private $argv;
 
     /** @param list<string>|null $argv */
     public function __construct(?array $argv)
     {
-        $this -> argv = $argv;
+        $this->argv = $argv;
     }
 
     public function read(): string
     {
-        return (is_array($this->argv) && isset($this->argv[1])) ? (string)$this->argv[1] : '';
+        return (is_array($this->argv) && isset($this->argv[1])) ? (string) $this->argv[1] : '';
     }
 }
