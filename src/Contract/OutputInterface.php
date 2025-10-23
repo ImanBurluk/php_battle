1f603b8e573afecab0423499552116e16f7529a4
ImanBurluk <jaroslavzev63@mail.ru>
2025-10-23 21:18:39 +0500

    refactor: Удалил комментарии в OutputInterface

--- src/Contract/OutputInterface.php
+++ src/Contract/OutputInterface.php
@@ -4,11 +4,7 @@ declare(strict_types=1);
 
 namespace Imanburluk\TextCount\Contract;
 
-/**
- * Выводит строку в целевой вывод (консоль, лог и т.д.).
- */
 interface OutputInterface
 {
-    /** Записывает строку в приёмник (консоль/файл/и т.д.) */
     public function write(string $line): void;
 }
