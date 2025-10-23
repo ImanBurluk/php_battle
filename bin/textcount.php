dc4f96e05407f48f9df963374e135523c5e10775
ImanBurluk <jaroslavzev63@mail.ru>
2025-10-23 21:16:51 +0500

    refactor: Навёл красоту в textcount

--- bin/textcount.php
+++ bin/textcount.php
@@ -5,11 +5,11 @@ declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use Imanburluk\TextCount\MbLengthCalculator;
-use Imanburluk\TextCount\TextCount;
 use Imanburluk\TextCount\Console\ArgvInput;
 use Imanburluk\TextCount\Console\TextCountApp;
 use Imanburluk\TextCount\IO\ConsoleOutput;
+use Imanburluk\TextCount\MbLengthCalculator;
+use Imanburluk\TextCount\TextCount;
 
 $app = new TextCountApp(
     new ArgvInput($argv),
